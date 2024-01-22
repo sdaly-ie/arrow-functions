7d7c9dd83a1d0b84dc4a09528f214bedd1d00cf2
sdaly-ie <>
2024-01-22 21:40:35 +0000

    Practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+/* function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum); */
 
 // Arrow Function With Parameters
+// Vanilla JavaScript Function
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => (a + b);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello There!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    '<p> This is a multiline string! </p>'
+)
+
+console.log(returnMultipleLines());
